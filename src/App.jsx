ff50f9bf8a595f43c56fc04984bb932ce4fb35ed
abigailjy90 <70828921+abigailjy90@users.noreply.github.com>
2021-10-18 20:53:29 +0900

    sharing props between routes

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,17 @@ import React from "react";
 import { BrowserRouter, HashRouter, Route } from "react-router-dom";
 import Home from "./routes/Home";
 import About from "./routes/About";
+import Detail from "./routes/Detail";
 import Navigation from "./components/Navigation";
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Navigation />
       <Route path="/" exact={true} component={Home} />
       <Route path="/about" component={About} />
-    </BrowserRouter>
+      <Route path="/movie/:id" component={Detail} />
+    </HashRouter>
   );
 }
 
